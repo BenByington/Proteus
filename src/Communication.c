93f11e40335a47c0d0ec18ede5228475583a7f43
BenByington <BenByington84@gmail.com>
2013-12-24 17:53:51 -0800

    Added License information for the project.  This software is now distributed under the GPL V3 licence

--- src/Communication.c
+++ src/Communication.c
@@ -1,3 +1,22 @@
+/*
+ * Copywrite 2013 Benjamin Byington
+ *
+ * This file is part of the IMHD software package
+ * 
+ * IMHD is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public Liscence as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * IMHD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with IMHD.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 /* 
  * File:   Communication.cpp
  * Author: Ben
