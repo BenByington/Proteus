b0c07334669c2db4f49bb683028f5601a2571575
BenByington <BenByington84@gmail.com>
2014-01-16 15:06:46 -0800

    Finally replaced IMHD with Proteus in all the lisence information etc

--- include/Logs/LogInfo.h
+++ include/Logs/LogInfo.h
@@ -1,20 +1,20 @@
 /*
  * Copywrite 2013 Benjamin Byington
  *
- * This file is part of the IMHD software package
+ * This file is part of the Proteus software package
  * 
- * IMHD is free software: you can redistribute it and/or modify it under the
+ * Proteus is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free 
  * Software Foundation, either version 3 of the License, or (at your option)
  * any later version.
  *
- * IMHD is distributed in the hope that it will be useful, but WITHOUT ANY
+ * Proteus is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
  * more details.
  *
  * You should have received a copy of the GNU General Public License along 
- * with IMHD.  If not, see <http://www.gnu.org/licenses/>
+ * with Proteus.  If not, see <http://www.gnu.org/licenses/>
  */
 
 #undef trace
